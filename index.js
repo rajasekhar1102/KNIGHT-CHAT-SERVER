23da0c1ac6517c6e97b21c8fc38b617acf7337ec
rajasekhar <raja1102nr@gmail.com>
2023-09-04 19:31:04 +0530

    added video feature

--- index.js
+++ index.js
@@ -9,19 +9,39 @@ app.use(cors());
 
 const server=http.createServer(app);
 
-const io=new Server(server,{cors:{origin:"https://knight-chat.onrender.com",},maxHttpBufferSize:1e8,  });
+const io=new Server(server,{cors:{origin:"https://knight-chat.onrender.com",methods:['GET','POST']},maxHttpBufferSize:1e8,  });
 
 
 io.on('connection',(socket)=>{
 
-socket.on("join_room",(data)=>{socket.join(data.room)});
+socket.on("join_room",(data)=>{socket.join(data.room);});
+
 
 socket.on("send_message",(msg)=>{
-
-    
      
     socket.to(msg.room).emit("receive_message",msg);
 });
+
+
+socket.on("callEnded",(data)=>{
+    io.to(data.room).emit("callEnd");
+})
+
+socket.on("callUser", (data)=>{
+    socket.to(data.room).emit("receiveCall");
+    setTimeout(() => {
+        socket.to(data.room).emit("callUser",{signal:data.signalData ,from:data.from,}) 
+    }, 2000);
+   
+
+})
+
+socket.on("answerCall",(data)=>{
+ 
+    socket.to(data.room).emit("callAccepted",data.signal);
+
+})
+
 
 
 });
